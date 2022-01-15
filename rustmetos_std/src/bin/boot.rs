4b220212cf1a109ca3dcc7afef07b31d836295af
xcodz-dot <71920621+xcodz-dot@users.noreply.github.com>
2022-01-15 20:04:34 +0530

    Rewrote cli in clap

--- rustmetos_std/src/bin/boot.rs
+++ rustmetos_std/src/bin/boot.rs
@@ -1,37 +1,39 @@
+use clap::{app_from_crate, arg, App};
 use configparser::ini::Ini;
 use rustmetos_api::api::path;
-use std::env::args;
+
+static VERSION: &str = "v1.0.0";
 
 fn main() {
-    let args: Vec<String> = args().collect();
-
-    if args.len() >= 2 {
-        if args[1] == "set" {
-            if args.len() >= 3 {
-                let command_name = &args[2];
-                let mut configuration = Ini::new();
-                configuration
-                    .load(&path("/conf/init.conf"))
-                    .unwrap_or_default();
-
-                configuration.remove_section("exec");
-                configuration.set("exec", "name", Some(command_name.clone()));
-
-                configuration
-                    .write(&path("/conf/init.conf"))
-                    .expect("Unable to write the modified configuration");
-            } else {
-                println!("{{binary_name}} required")
-            }
-        } else if args.get(1).unwrap_or(&String::from("help")) == &"help" {
-            println!(
-                "\
-Usage: boot [set] [args...]
-
-Commands:
-  set {{binary_name}}      Set boot binary
-  help                   Show this message"
+    let app = app_from_crate!()
+        .about("Utility to set boot options in `init.conf`.")
+        .version(VERSION)
+        .subcommand(
+            App::new("set")
+            .about("Set boot options in the configuration.")
+            .arg(
+                arg!(
+                    <executable> "Name of the executable to set as boot. (Must be inside the /bin directory)"
+                )
             )
+        );
+    let args = app.get_matches();
+
+    match args.subcommand() {
+        Some(("set", sub_args)) => {
+            let command_name = String::from(sub_args.value_of("executable").unwrap());
+            let mut configuration = Ini::new();
+            configuration
+                .load(&path("/conf/init.conf"))
+                .unwrap_or_default();
+
+            configuration.remove_section("exec");
+            configuration.set("exec", "name", Some(command_name.clone()));
+
+            configuration
+                .write(&path("/conf/init.conf"))
+                .expect("Unable to write the modified configuration");
         }
+        _ => (),
     }
 }
