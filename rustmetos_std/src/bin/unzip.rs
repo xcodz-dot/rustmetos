0de43bcb2c220869626592188ba9d3cd40e2b037
xcodz-dot <71920621+xcodz-dot@users.noreply.github.com>
2022-01-15 22:31:49 +0530

    Converted unzip to clap cli

--- rustmetos_std/src/bin/unzip.rs
+++ rustmetos_std/src/bin/unzip.rs
@@ -1,19 +1,29 @@
-use std::env::args;
+use clap::{arg, App};
 use std::fs::File;
 
+static VERSION: &str = "v1.0.0";
+
 fn main() {
-    let args: Vec<String> = args().collect();
+    let app = App::new("unzip")
+        .about("Extracts zip archives into a destination folder.")
+        .version(VERSION)
+        .arg(arg!(
+            <zipfile> "Zip file to extract."
+        ))
+        .arg(arg!(
+            <destination> "Destination directory to extract to."
+        ));
 
-    if args.len() <= 2 {
-        println!("Not enough arguments to unzip\nplease provide a file to extract and a directory to extract to.");
-        return;
-    }
+    let args = app.get_matches();
 
-    let archive_path = &args[1];
+    let archive_path = args.value_of("zipfile").expect("Zip file is required");
     let archive_file = File::open(archive_path).unwrap();
     let mut archive = zip::ZipArchive::new(archive_file).unwrap();
 
     archive
-        .extract(&args[2])
+        .extract(
+            args.value_of("destination")
+                .expect("Destination is required"),
+        )
         .expect("Archive extraction unsuccesful");
 }
